07d855b08b9c00530551cd85c91e5d7d0956242b
akanazawa <angjoo.kanazawa@gmail.com>
2011-10-27 22:46:21 -0400

    make unique words

--- Ran/makeDictionary.py
+++ Ran/makeDictionary.py
@@ -2,8 +2,21 @@
 Goes through each file and makes a set of unique words
 Write it down to file
 """
-
-stopwords = open('stopwords.txt', 'r').read().split()
-
+from sets import Set
+# get stop words
+stopWords = open('stopwords.txt', 'r').read().split()
+# files to go through
 texts = ['graphics-windows-test.fastdt', 'graphics-windows-train.fastdt'];
-possibleWords = open('
+# the set
+uniqueWords = Set()
+for f in texts:
+    possibleWords = open(f, 'r').read().split()
+    for w in possibleWords:
+        if w not in stopWords or not w.isdigit():
+            uniqueWords.add(w)
+
+output = open('uniqueWords.txt', 'w')
+for w in uniqueWords:
+    output.write(w+'\n')
+
+output.close()
