36d0609385a4071ecca61140438559dfe6442f55
akanazawa <angjoo.kanazawa@gmail.com>
2011-10-27 23:20:52 -0400

    almost done with unique words

--- Ran/makeDictionary.py
+++ Ran/makeDictionary.py
@@ -2,20 +2,31 @@
 Goes through each file and makes a set of unique words
 Write it down to file
 """
-from sets import Set
+import Stemmer
+
 # get stop words
 stopWords = open('stopwords.txt', 'r').read().split()
 # files to go through
 texts = ['graphics-windows-test.fastdt', 'graphics-windows-train.fastdt', 'train.basebal.hockey.fastdt'];
-# the set
-uniqueWords = Set()
+# the dictionary
+uniqueWords = dict()
+stem = Stemmer.Stemmer('english')
 for f in texts:
     print "looking at " + f
     possibleWords = open(f, 'r').read().split()
     for w in possibleWords:
         if w not in stopWords or not w.isdigit():
-            uniqueWords.add(w)
+            w = stem.stemWord(w)
+            if w in uniqueWords.keys():
+                uniqueWords[w] +=1 # add count
+            else:
+                uniqueWords[w] = 1
 
+for word, count in uniqueWords:
+    if count < 4:
+        print "delete: " + word
+        del uniqueWords[word]
+        
 output = open('uniqueWords.txt', 'w')
 for w in uniqueWords:
     output.write(w+'\n')
