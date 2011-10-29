4b130fc2d52d3f97e02f0f947bccd0c8a4c96b2c
Angjoo <kanazawa@umiacs.umd.edu>
2011-10-29 17:58:17 -0400

    update writeup, made wu5_tre for write up question 5

--- startup.py
+++ startup.py
@@ -20,15 +20,15 @@ x, trajectory = gd.gd(lambda x: x**2, lambda x:2*x, 10, 100, 0.2)
 #  else: print('converges to x:', x, ', at step:', i);
 
 # WU 2 ##########
-# f = lambda x: sin(pi*x) + x**2/2
-# derF = lambda x: pi*cos(pi*x) + x
+f = lambda x: sin(pi*x) + x**2/2
+derF = lambda x: pi*cos(pi*x) + x
 # x = linspace(-5, 5, 500)
 # plot(x, f(x), 'b-')
 # plot(-0.4538, f(-0.4538), 'r*') #plot the global min
 # title('f(x) = sin(x*pi) + x^2/2')
 
-# x_global, t = gd.gd(f, derF, 0, 10, 0.2)
-# x_local, t = gd.gd(f, derF, 1, 10, 0.2)
+x_global, t = gd.gd(f, derF, 0, 10, 0.2)
+x_local, t = gd.gd(f, derF, 1, 10, 0.2)
 
 ##########
 
